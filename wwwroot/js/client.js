c6dac238f6c5fa4f31b70cdd594c86e6a9c06db6
Joshua Rauch <jrauch2@my.wctc.edu>
2019-02-07 14:03:39 -0600

    hide toast on escape, intellisense nodes

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,6 +1,7 @@
 ﻿$(function(){
     // preload audio
     var toast = new Audio('media/toast.wav');
+    var keyCodeHideToast = 27;
 
     $('.code').on('click', function(e) {
         e.preventDefault();
@@ -11,5 +12,11 @@
         // play audio
         toast.play();
         $('#toast').toast({ autohide: false }).toast('show');
+    });
+
+    $('body').keydown(function (e) {
+        if (e.originalEvent.keyCode == keyCodeHideToast) {
+            $('#toast').toast('hide');
+        }
     });
 });
