2af13f504d624a0fb349ec4c81c9d607021dd316
Joshua Rauch <jrauch2@my.wctc.edu>
2019-02-14 07:10:44 -0600

    added content to remaining grid items. display product name and discount code in the toast using data-attributes.

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,9 +1,13 @@
-﻿$(function(){
-    // preload audio
-    var toast = new Audio('media/toast.wav');
+﻿/*global $, toast*/
+
+$(function () {
+    // pre-load audio
+    var toast = new Audio("media/toast.wav");
+    // keydown to hide toast on
     var keyCodeHideToast = 27;
 
-    $('.code').on('click', function(e) {
+
+    $(".code").on("click", function(e) {
         e.preventDefault();
         // first pause the audio (in case it is still playing)
         toast.pause();
@@ -11,12 +15,21 @@
         toast.currentTime = 0;
         // play audio
         toast.play();
-        $('#toast').toast({ autohide: false }).toast('show');
+
+        // set the value of the toast data
+        $("#toast").data("name", $(this).parent().data("product").name);
+        $("#toast").data("code", $(this).parent().data("code"));
+        $("#product").text($("#toast").data("name"));
+        $("#code").text($("#toast").data("code"));
+
+        // show toast
+        $("#toast").toast({ autohide: false }).toast("show");
     });
 
-    $('body').keydown(function (e) {
-        if (e.originalEvent.keyCode == keyCodeHideToast) {
-            $('#toast').toast('hide');
+    // hide toast on keydown
+    $("body").keydown(function (e) {
+        if (e.originalEvent.keyCode === keyCodeHideToast) {
+            $("#toast").toast("hide");
         }
     });
 });
