78fb22605b29b9ace9f8afcbcb754c2a37a39140
Joshua Rauch <jrauch2@my.wctc.edu>
2019-02-14 14:54:06 -0600

    Homework done.

--- wwwroot/js/balloons.js
+++ wwwroot/js/balloons.js
@@ -1,4 +1,6 @@
-﻿$(function(){
+﻿/* global $ */
+$(function () {
+    var rand = Math.floor(Math.random() * 10);
     $('#birthday').pickadate({ format: 'mmmm, d' });
 
     // uncheck all checkboxes (FireFox)
@@ -11,7 +13,46 @@
         $('#' + this.id + 'Img').css('visibility', 'visible')
         // animate balloon in/out based on checkbox
         $(this).is(':checked') ?
-         $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
-         $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+         $('#' + this.id + 'Img').removeClass().addClass('animated slideInUp') :
+         $('#' + this.id + 'Img').addClass('animated slideOutDown slow');
+    });
+    // Randomize the attention seeker animation
+    var attentionSeekers = [
+        "bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello", "heartBeat"
+    ];
+    $("h1.animated").addClass(attentionSeekers[rand]);
+    // submit button toast
+    $("#submit").click(function() {
+        var checked = [];
+        $('.form-check-input').each(function () {
+            if ($(this).prop('checked')) {
+                checked.push($(this));
+            }
+        });
+        if (checked.length < 1) {
+            $('#toast').toast({ autohide: false }).toast('show');
+        }
+    });
+    // hide toast on escape 
+    $(document).on('keyup', function (e) {
+        if (e.key === "Escape") {
+            $('#toast').toast('hide');
+        }
+    });
+    // toggle all balloons
+    $('#all').click(function() {
+        $('.form-check-input').each(function () {
+            if ($('#all').prop('checked')) {
+                $(this).prop('checked', true).trigger('change');
+            } else {
+                $(this).prop('checked', false).trigger('change');
+            }
+        });
+    });
+    // change color on hover
+    $('.form-check-label').hover(function () {
+        $('h1.animated').css('color', $(this).get(0).htmlFor);
+    }, function() {
+        $('h1.animated').css('color', "slategrey");
     });
 });
