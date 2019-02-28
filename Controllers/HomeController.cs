08da5616e4829e33ec0c87f75661d7dd5910ed64
Joshua Rauch <jrauch2@my.wctc.edu>
2019-02-28 15:13:39 -0600

    Done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Core.Models;
 
 namespace Northwind.Core.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index() => View();
+        // this controller depends on the BloggingRepository
+        private INorthwindRepository repository;
+        public HomeController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Index() => View(repository.Categories.Include(c => c.Products).OrderBy(c => c.CategoryName).ToList());
     }
 }
---
+++ Models/INorthwindRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Northwind.Core.Models;
+
+namespace Northwind.Core.Models
+{
+    public interface INorthwindRepository
+    {
+        IQueryable<Category> Categories { get; }
+        IQueryable<Product> Products { get; }
+
+        // TODO: AddBlog, UpdateBlog, DeleteBlog
+        // TODO: AddPost, UpdatePost, DeletePost
+    }
+}
