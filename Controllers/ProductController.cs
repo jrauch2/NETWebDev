12f20dc5d83848c0d314a72d6ef2f971ff6bcbee
Joshua Rauch <jrauch2@my.wctc.edu>
2019-03-14 10:20:02 -0500

    Added Category Views Added sound to toast Added Product Controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Core.Models;
+
+namespace Northwind.Core.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the BloggingRepository
+        private INorthwindRepository repository;
+        public ProductController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Category() => View(repository.Categories.OrderBy(c => c.CategoryName));
+        public IActionResult CategoryDetail(int id) => View(repository.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+    }
+}
